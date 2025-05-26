160c657266acfe73d1de8149df659da65debef79
Rajnish8292 <rajnish81018@gmail.com>
2025-05-26 20:30:19 +0530

    change in server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express()
 const path = require("path");
 const PORT = 3000;
-app.listen(2301);
+app.listen(2301, () => {
+    console.log(`Development server started at PORT 2301`)
+});
 
 
 app.get("/", (req, res) => {
